84bfa28e1fecd8b17591e83e4040bcefc453ead7
faisalSid12 <55405247+faisalSid12@users.noreply.github.com>
2023-05-18 16:20:51 +0500

    Delete awsConfig.js

--- src/awsConfig.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-    region: "us-east-1",
-    accessKeyId: "AKIAWN7PP6TDPG6NKS7C",
-    secretAccessKey: "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",
-    bucketName: "nkenne.dev",
-    folderName: "audios"
-};
